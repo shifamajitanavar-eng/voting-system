73e988beac76a71a43a83e1cd4fb5607d7ec7d42
shifamajitanavar-eng <shifamajitanavar@gmail.com>
2026-09-12 11:58:10 +0530

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,5 +1,7 @@
 age=int(input("enter your age"))
 if age>=18:
   print("your are eligible to vote")
+  print("you can cast your vote")
 else:
   print("your are niot eligible")
+    print("you cannot cast your vote")
